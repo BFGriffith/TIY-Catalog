c936e91964b539058aa0d19fd8cfcdf87bc2018c
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-06-17 16:02:45 -0400

    curling store and listing apis

---
+++ api/shop/undulatingcontours.js
@@ -0,0 +1,38 @@
+{
+  "count": 1,
+  "results": [{
+    "shop_id": 7321706,
+    "shop_name": "undulatingcontours",
+    "user_id": 25128870,
+    "creation_tsz": 1348358713,
+    "title": "Modern Apple and Android docking stations ",
+    "announcement": "Industrial chic Apple and Android docking stations from wood etc. \r\nUndulating Contours creates functional pieces for modern-day living.\r\n\r\n\r\n\r\n",
+    "currency_code": "USD",
+    "is_vacation": false,
+    "vacation_message": "We will be back soon. Please excuse us while we make some changes to better serve our customers. ",
+    "sale_message": "I&#39;m always excited to hear back from someone who has purchased one of my designs. Once you&#39;ve received your item, please let me know your thoughts and if there&#39;s anything that would have made your experience better. \r\n\r\n\r\n\r\nThanks, \r\nChris    ",
+    "digital_sale_message": null,
+    "last_updated_tsz": 1434569843,
+    "listing_active_count": 32,
+    "login_name": "Fandz",
+    "accepts_custom_requests": false,
+    "policy_welcome": null,
+    "policy_payment": "Currently I accept PayPal and Credit Card Payments through Etsy. If you need to pay by money order, contact me and I&#39;ll see what we can work out. ",
+    "policy_shipping": " I ship USPS priority for domestic US orders and USPS first class international orders. Most listings should have USPS rates already listed. If shipping is not listed to your location, please send me a convo and I&#39;ll get you a quote.   \r\n\r\nI try to ship out orders between 3 to 5 days after purchase. However, due to fluctuations in demand, the official turnaround is 1 to 2 weeks after your order is placed. If you need to receive an order by a certain date, please send a convo with the requested date so I may make arrangements.  \r\n\r\nNote to International customers:\r\n\r\nYour delivery country may charge taxes or duties on your purchase.  You will be responsible for paying these charges and as taxes vary by nation I connot pay these or instruct you on their amount. You should also be aware that customs processing times may vary throughout the year and it may delay your package considerably. Be mindful of busy shipping seasons around holidays. If you have any questions on customs and their processing times, please contact your local customs office.\r\n\r\n\r\n",
+    "policy_refunds": "Full return minus shipping for any item returned within 14 days after item has been received. Please contact me before you ship.   ",
+    "policy_additional": "Do you have something special in mind? Besides bringing my own ideas to life, I enjoy bringing the creative ideas of others into fruition. \r\n\r\nI&#39;m open to discussing wholesale and consignment orders. If you have your own shop and would like to carry some of my items, or items specifically designed for your shop needs, send me message and I&#39;ll see what we can work out. \r\n\r\nWhat to expect?\r\nWood isn&#39;t like metal or plastic where every piece can be indistinguishable by the eye. Some of my items will have nicks from knots in the wood and from general machining. To me these are beauty marks of the material and process. Unless noted, the items I sell  will be as pictured. If a piece isn&#39;t to your standards, please return withing 14 days for a refund minus shipping. ",
+    "policy_seller_info": null,
+    "policy_updated_tsz": 1410909961,
+    "vacation_autoreply": null,
+    "url": "https:\/\/www.etsy.com\/shop\/undulatingcontours?utm_source=tiycatalog&utm_medium=api&utm_campaign=api",
+    "image_url_760x100": "https:\/\/img0.etsystatic.com\/022\/0\/7321706\/iusb_760x100.12361306_rc4g.jpg",
+    "num_favorers": 5120,
+    "languages": ["en-US"],
+    "upcoming_local_event_id": null
+  }],
+  "params": {
+    "shop_id": "undulatingcontours"
+  },
+  "type": "Shop",
+  "pagination": {}
+} %
