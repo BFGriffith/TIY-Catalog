0b387bbe0bd7ff186d98c0332678341661de6fa7
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-06-18 19:10:56 -0400

    Placeholders starting point.

---
+++ src/js/category.js
@@ -0,0 +1,21 @@
+(function(window){
+
+  $.getJSON("../../api/product-detail/img/images_main_img.json").then(function(mainImage) {
+    console.log(mainImage);
+
+    $image = $('.image_1'),
+
+
+
+
+  });//end of main image fn.
+
+
+
+
+
+
+
+
+
+})(window);
