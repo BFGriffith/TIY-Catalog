2f571b833e3389d04ad6b46fee87a2ffad5f1eb6
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-19 15:48:32 -0400

    more styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
-(function (window) {
+(function(window) {
+
+  function product(data) {
+
+    return {
+
+      shop_name: data.shop_name,
+
+
+
+    };
+  }
+
+  new Vue({
+    el: 'shop_name',
+    data: {
+
+      shop_name: product.shop_name
+
+    }
+
+
+  });
 
 
 
