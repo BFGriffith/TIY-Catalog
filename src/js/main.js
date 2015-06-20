b4fa7e2d6cf55ce6821a832ab27f545d9f110060
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-20 13:39:14 -0400

    Just moving along

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,26 @@
-(function (window) {
+(function(window) {
 
 
+  $.getJSON('../../api/product-detail/shop/undulatingcontours.json')
+    .then(function(response) {
+
+
+
+      // function product(result) {
+      //   return {
+      //
+      //     title: result.shop_name
+      //   };
+      // }
+      //
+      //
+      // new Vue({
+      //   el: '.shopTitle',
+      //   data: {
+      //     shop_name: product(response.result[0].shop_name)
+      //   }
+      //
+      // })
+    });
 
 })(window);
