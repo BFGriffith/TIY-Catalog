376d15c39b551c36123376ea3c5275845288faf5
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-06-20 13:50:15 -0400

    re-structuring html, semantics and best practices

--- src/js/category.js
+++ src/js/category.js
@@ -1,36 +1,67 @@
-// (function(window){
-//
-//   $.getJSON('../product-detail/img/product-detail/img/images_main_img.json')
-//   .then(function(response)
-//
-//       new Vue({
-//         el: '.image'
-//       })
+// (function(){
+//   $.getJSON('/api/listings/113244585.json?api_key=REDACTED&includes=Blah,Blah,Blah')
+//     .then(function(response){
 //
 //
-
-
-
-      // function response(result) {
-      //   return {
-
-      //     main_image_url: result.MainImage.url_170x135
-      //   };
-      // }
-
-
-
-      // var $images_col_1 = $('img','.img1');
-      //     $img1.attr('src', allImages.url_170x135_url );
-
-        // $image = $('img.image_col_1_descr');
-        // $image.attr('src', 'images_main_img.image_url');
-        // $image.attr('href', 'https:\/\/img0.etsystatic.com\/062\/0\/7321706\/il_170x135.786749714_hlug.jpg')
-
-
-  // });//end of main image fn.
-
-
-
-
-)(window);
+//
+//
+// 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// // (function(window){
+// //
+// //   $.getJSON('../product-detail/img/product-detail/img/images_main_img.json')
+// //   .then(function(response)
+// //
+// //       new Vue({
+// //         el: '.image'
+// //       })
+// //
+// //
+//
+//
+//
+//       // function response(result) {
+//       //   return {
+//
+//       //     main_image_url: result.MainImage.url_170x135
+//       //   };
+//       // }
+//
+//
+//
+//       // var $images_col_1 = $('img','.img1');
+//       //     $img1.attr('src', allImages.url_170x135_url );
+//
+//         // $image = $('img.image_col_1_descr');
+//         // $image.attr('src', 'images_main_img.image_url');
+//         // $image.attr('href', 'https:\/\/img0.etsystatic.com\/062\/0\/7321706\/il_170x135.786749714_hlug.jpg')
+//
+//
+//   // });//end of main image fn.
+//
+//
+//
+//
+// )(window);
