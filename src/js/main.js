9716bc98c2a1f91d29faf086daa2b2841aeb85db
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-06-22 08:21:09 -0400

    Vue placeholders still do not work

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,23 @@
 (function(window) {
+  $.getJSON('../../api/product-detail/listings/210529014.json')
+            .then(function(placeholderList) {
 
+                $storeTitle = $('#storename');
+                $storeTitle.text(placeholderList.results[0].Shop.shop_name);
 
+                $storePic = $('a', 'img#shopLogo');
+                $storePic.attr('src', placeholderList.results[0].Images.url_75x75);
+
+                $productTitle = $('#productinfo');
+                $productTitle.text(placeholderList.results[0].title)
+
+                $productCost = $('#cost');
+                $productCost.text(placeholderList.results[0].price)
+
+            });
+/*
   $.getJSON("../api/product-detail/shop/undulatingcontours.json")
     .then(function(results) {
-      console.log(hello);
-
-
-
-
     });
-
+*/
 })(window);
