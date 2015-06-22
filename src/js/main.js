9603621d10edd84fbb3735a60b4a54b65836c7d4
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-06-22 09:15:41 -0400

    fixed merge conflict duplication

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-(function(window){
-=======
 (function(window) {
 
 
   $.getJSON("../api/product-detail/shop/undulatingcontours.json")
     .then(function(results) {
       console.log(hello);
->>>>>>> 4b5440b4001353c3e7fd2e81fb4606a74618be2d
-
-
-
 
     });
 
